2befaad0b3028b2e22a58a82e69ba2f0fefd86ee
Troy Wang <troyerwang.dev@gmail.com>
2024-04-03 12:05:22 +0800

    1_引流素材自动化代码提交

---
+++ 1_引流素材自动化/create_video.py
@@ -0,0 +1,82 @@
+from moviepy.editor import *
+import random
+import os
+
+
+def yl_video(video_path, wx_text):
+    # 打开视频剪辑
+    with VideoFileClip(video_path) as video_clip:
+        # 裁剪前5秒并调整大小
+        cropped_clip = video_clip.subclip(0, 5).resize((400, 255))
+
+        # 创建文本剪辑
+        text_clip_1 = TextClip("薇", fontsize=30, color="white", font='Microsoft-YaHei-&-Microsoft-YaHei-UI')
+        text_clip_2 = TextClip("❤️", fontsize=30, color="white", font='Segoe-UI-Symbol')
+        text_clip_3 = TextClip(f":{wx_text}", fontsize=30, color="white", font='Microsoft-YaHei-&-Microsoft-YaHei-UI')
+
+        # 计算文本的宽度
+        text_width = sum([text_clip_1.w, text_clip_2.w, text_clip_3.w])
+
+        # 计算文本居中的位置
+        center_x = (cropped_clip.w - text_width) / 2
+
+        # 设置每个文本剪辑的位置
+        text_clip_1 = text_clip_1.set_position((center_x, 100)).set_duration(5)
+        text_clip_2 = text_clip_2.set_position((center_x + text_clip_1.w, 100)).set_duration(5)
+        text_clip_3 = text_clip_3.set_position((center_x + text_clip_1.w + text_clip_2.w, 100)).set_duration(5)
+
+        arrow_clip = VideoFileClip("assets/gif/arrow.gif")
+        arrow_clip = arrow_clip.resize((50, 50))
+
+        looped_arrow_clip = arrow_clip.fx(vfx.loop, duration=5)
+
+        # 设置 GIF 图像的位置
+        looped_arrow_clip = looped_arrow_clip.set_position((center_x, 50)).set_duration(5)
+
+        # 将视频剪辑和文本剪辑组合在一起
+        composite_clip = CompositeVideoClip([cropped_clip, text_clip_1, text_clip_2, text_clip_3, looped_arrow_clip])
+
+        # 创建保存图片的文件夹
+        output_directory = "output_videos"
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        # 将组合后的视频剪辑写入文件
+        output_path = os.path.join(output_directory, f"{wx_text}.mp4")
+        composite_clip.write_videofile(output_path, codec="libx264")
+
+        # 关闭视频剪辑
+        arrow_clip.close()
+        cropped_clip.close()
+        text_clip_1.close()
+        text_clip_2.close()
+        text_clip_3.close()
+        looped_arrow_clip.close()
+        composite_clip.close()
+
+
+def get_video_template():
+    template_dir = 'E:/PythonProject/1000PythonCode/1_引流素材自动化/assets/videos'
+    template_files = []
+    for root, dirs, files in os.walk(template_dir):
+        for file in files:
+            template_files.append(os.path.join(root, file))
+    return template_files
+
+
+def get_txt_wx_account():
+    wx_accounts = []
+    with open('conf/wx_accounts.txt', 'r') as file:
+        for line in file.readlines():
+            wx_accounts.append(line.strip())
+    return wx_accounts
+
+
+if __name__ == '__main__':
+    wx_array = get_txt_wx_account()
+    print(wx_array)
+    pass
+    my_array = get_video_template()
+    for item_text in wx_array:
+        yl_video(random.choice(my_array), item_text)
+        print(f"已为您生成微信号为 {item_text} 的引流视频！")
---
+++ README.md
@@ -0,0 +1,3 @@
+#1000PythonCode
+
+目的：学习Python，使用Python为工作提高效率
